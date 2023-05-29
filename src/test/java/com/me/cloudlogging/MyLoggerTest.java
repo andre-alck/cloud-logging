c38a441635b11a95230655d25540d20a62451022
andre-alck <andrealck1@gmail.com>
2023-05-28 21:51:33 -0300

    test: utiliza appenders para verificacao de logs

--- src/test/java/com/me/cloudlogging/MyLoggerTest.java
+++ src/test/java/com/me/cloudlogging/MyLoggerTest.java
@@ -1,13 +1,75 @@
 package com.me.cloudlogging;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 
 public class MyLoggerTest {
 	
 	@Test
-	public void givenMyLoggerWhenCalledLogThenShouldLog() {
-		MyLogger myLogger = new MyLogger();
-        myLogger.log();
+	public void givenMyLoggerWhenCalledInfoLogThenShouldLogAnInformationAndVerifyItThroughAnAppender() {
+		// preparation
+		ListAppender<ILoggingEvent> appenders = new ListAppender<>();
+		appenders.start();
+		
+		Logger logger = (Logger) LoggerFactory.getLogger(MyLogger.class);
+		logger.addAppender(appenders);
+		
+		// action
+		MyLogger.infoLog();
+		
+		List<ILoggingEvent> logs = appenders.list;
+		
+		// verification
+		assertEquals("mylog.logger.info", logs.get(0).getMessage());
+		assertEquals(Level.INFO, logs.get(0).getLevel());
 	}
+	
+	@Test
+	public void givenMyLoggerWhenCalledErrorLogThenShouldLogAnInformationAndVerifyItThroughAnAppender() {
+		// preparation
+		ListAppender<ILoggingEvent> appenders = new ListAppender<>();
+		appenders.start();
+		
+		Logger logger = (Logger) LoggerFactory.getLogger(MyLogger.class);
+		logger.addAppender(appenders);
+		
+		// action
+		MyLogger.errorLog();
+		
+		List<ILoggingEvent> logs = appenders.list;
+		
+		// verification
+		assertEquals("mylog.logger.error", logs.get(0).getMessage());
+		assertEquals(Level.ERROR, logs.get(0).getLevel());
+	}
+	
+	@Test
+	public void givenMyLoggerWhenCalledDebugLogThenShouldNotLogAsItIsBelowThreshold() {
+		// preparation
+		ListAppender<ILoggingEvent> appenders = new ListAppender<>();
+		appenders.start();
+		
+		Logger logger = (Logger) LoggerFactory.getLogger(MyLogger.class);
+		logger.addAppender(appenders);
+		
+		// action
+		MyLogger.debugLog();
+		
+		List<ILoggingEvent> logs = appenders.list;
+		
+		// verification
+		assertTrue(logs.isEmpty());
+	}
+	
 
 }
